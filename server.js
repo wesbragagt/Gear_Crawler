7309b1e55528a8cf39428e5d251c9761b00b37ab
weslley braga <wesbragagt@gmail.com>
2019-10-15 23:08:05 -0500

    sorting by price

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var cheerio = require('cheerio');
 var axios = require('axios');
 
+// ===============================CALL==========================================
+getPedals(getCheerioElements);
+// ================================FUNCTIONS===========================================
 async function getPedals(next) {
+	const query = process.argv
+		.splice(2)
+		.join(' ')
+		.replace(/\s/g, '%20');
+
+	console.log('query: ', query);
 	const result = await axios.get(
-		'https://reverb.com/marketplace?query=tube%20screamer'
+		`https://reverb.com/marketplace?query=${query}&condition=used`
 	);
 
 	next(result);
 }
-
 function getCheerioElements(result) {
 	const $ = cheerio.load(result.data);
 	const output = [];
@@ -17,15 +25,27 @@ function getCheerioElements(result) {
 			.children()
 			.text();
 		const link = $(element).attr('href');
-		const price = '$' + title.substr(title.indexOf('$') + 1);
+		const condition = $(element)
+			.children('.condition-indicator__label')
+			.text();
+		let price = title.substr(title.indexOf('$') + 1);
+		price = parseFloat(price).toFixed(2);
+
 		output.push({
 			index,
 			title,
 			link,
-			price
+			price,
+			condition
 		});
 	});
-	console.log(output);
+	console.log(output.sort((a, b) => a.price - b.price));
 }
 
-getPedals(getCheerioElements);
+function filterByPriceRange(arr, [min, max]) {
+	return arr.filter(e => e.price >= min && e.price <= max);
+}
+
+function sortByPriceAsc(arr) {
+	return arr.sort((a, b) => a.price - b.price);
+}
